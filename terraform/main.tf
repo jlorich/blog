7ca4222d96885fa736541c8ce1e69b69356c3e49
Joey Lorich <joseph@Lorich.me>
2019-07-23 17:27:56 -0600

    configure static site outside of terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,13 +23,8 @@ resource "azurerm_storage_account" "default" {
   account_tier              = "Standard"
   account_replication_type  = "LRS"
   enable_https_traffic_only = true
-
-  provisioner "local-exec" {
-    command = "az storage blob service-properties update --account-name ${azurerm_storage_account.default.name} --static-website  --index-document index.html --404-document 404.html"
-  }
 }
 
 output "storage_account" {
   value = azurerm_storage_account.default.name
 }
-
