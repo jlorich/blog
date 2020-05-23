68c7354a15ae4f792e0c9bc86a418fef85eaebb5
Joey Lorich <joseph@lorich.me>
2020-05-23 12:23:36 -0600

    ci updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,9 @@ resource "azurerm_storage_account" "default" {
   account_tier              = "Standard"
   account_replication_type  = "LRS"
   enable_https_traffic_only = true
+  static_website {
+    index_document = "index.html"
+  }
 }
 
 output "storage_account" {
