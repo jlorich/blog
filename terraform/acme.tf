18c44fc1a244a5b351259069f6ec8d637f8358d3
Joey Lorich <joseph@lorich.me>
2020-05-25 18:53:03 -0600

    uno mas

--- terraform/acme.tf
+++ terraform/acme.tf
@@ -68,7 +68,7 @@ resource "azurerm_key_vault_certificate" "default" {
 
   certificate_policy {
     issuer_parameters {
-      name = "Fake LE Intermediate X1"
+      name = "CN=Fake LE Root X1"
     }
 
     key_properties {
