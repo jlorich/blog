3e87a03929f9c8f42a01aef41c79408ca9e46b79
Joey Lorich <joseph@lorich.me>
2020-06-05 13:02:00 -0600

    Remove example kv policy

--- terraform/acme.tf
+++ terraform/acme.tf
@@ -70,21 +70,6 @@ resource "azurerm_key_vault_access_policy" "cdn" {
   ]
 }
 
-resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id = azurerm_key_vault.example.id
-
-  tenant_id = "00000000-0000-0000-0000-000000000000"
-  object_id = "11111111-1111-1111-1111-111111111111"
-
-  key_permissions = [
-    "get",
-  ]
-
-  secret_permissions = [
-    "get",
-  ]
-}
-
 resource "azurerm_key_vault_certificate" "default" {
   name         = "${replace(var.domain, ".", "_")}-${substr(var.environment, 0, 2)}"
   key_vault_id = azurerm_key_vault.default.id
